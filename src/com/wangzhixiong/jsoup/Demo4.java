cb37fb56a41c61378d771caf04a94f7a32cc6942
yunshu-cloud <w2099266050@gmail.com>
2023-08-13 16:11:10 +0800

    jsoup解析器_Element

---
+++ src/com/wangzhixiong/jsoup/Demo4.java
@@ -0,0 +1,26 @@
+package com.wangzhixiong.jsoup;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Demo4
+{
+  // Document
+  public static void main(String[] args) throws IOException
+  {
+    String path = Demo4.class.getClassLoader().getResource("com/wangzhixiong/jsoup/student.xml").getPath();
+    Document document = Jsoup.parse(new File(path), "utf-8");
+
+    Elements elements = document.select("#baizhan_0001");
+    Element element =elements.get(0);
+    System.out.println(element.text());
+    System.out.println(element.html());
+    System.out.println(element.attr("id"));
+  }
+}
+
